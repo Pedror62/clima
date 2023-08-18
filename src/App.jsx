095d0f59ca04ae2e3f1c5a187ddddb9fcc5562c2
pedror62 <102195011+Pedror62@users.noreply.github.com>
2023-08-17 21:00:11 -0300

    atualizações do fomr

--- src/App.jsx
+++ src/App.jsx
@@ -19,49 +19,74 @@ function App() {
 
   async function buscarcitesbnt(cityes) {
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityes}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json());
-
-
     dadosIntela(dados);
+
+
+    console.log(dados)
   }
 
 
   function clickbnt() {
     const cityes = document.querySelector(".text").value;
-
+  
     buscarcitesbnt(cityes)
-
-
+   
+   
   }
+
+  
+  // const buttonLupa = document.getElementsByClassName('buttonLupa');
+
+
+  buttonLupa.addEventListener("click", () => {
+    console.log("apertou")
+  });
+
+
+  // function lond() {
+  //   const buttonLupa = document.getElementsByClassName('buttonLupa')
+  //   buttonLupa.addEventListener("click", () => {
+  //     console.log("apertou")
+  //   });
+
+  //   lond() 
+  // }
+
+
+
+
 
   return (
     <>
-      <div className='App'>
-        <div className='container'>
-          <h1>Previsão do tempo</h1>
-          <div className='content'>
 
-            <input type="text" className='text' placeholder='Digite Sua Cidade...' />
-            <button className='buttonLupa' onClick={clickbnt}>
-              <i className="fa-solid fa-magnifying-glass" ></i>
-            </button>
+        <div className='App'>
+          <h1 className='fora'>Previsão do tempo</h1>
+          <div className='container'>
 
-            <div className='results' >
+            <div className='content'>
+              <h1>Consultar Clima</h1>
+              <input type="text" className='text' placeholder='Digite Sua Cidade...' required />
+              <button className='buttonLupa' onClick={clickbnt}>
+                <i className="fa-solid fa-magnifying-glass" ></i>
+              </button>
 
-              <h1 className='city'>Tempo em...</h1>
-              <p className='temperatura'></p>
+              <div className='results' >
 
-              <div className='resultTemp'>
-                <img className="imagPrevisao" src="https://openweathermap.org/img/wn/04n.png" alt="icon-tempo" />
-                <p className='textPrevisao'></p>
+                <h1 className='city'>Tempo em...</h1>
+                <p className='temperatura'></p>
+
+                <div className='resultTemp'>
+                  <img className="imagPrevisao" src="https://openweathermap.org/img/wn/04n.png" alt="icon-tempo" />
+                  <p className='textPrevisao'></p>
+                </div>
+
+                <p className='umid'>unidade...</p>
+
               </div>
-
-              <p className='umid'>unidade...</p>
-
             </div>
           </div>
         </div>
-      </div>
-
+    
     </>
   )
 }
